406c292ac5892e594019236cd0b2c3b1d3faa573
ompgupta <68551688+ompgupta@users.noreply.github.com>
2021-09-15 17:09:21 +0530

    done

---
+++ src/components/StudentsApi.js
@@ -0,0 +1,110 @@
+const StudentsData = [
+    {
+     id: "1",
+     name: "Ram Singh",
+     class: "11th",
+     gender :"Male",
+     marks: "60"
+    },
+
+    {
+        id: "2",
+        name: "Rohan Raj",
+        class: "10th",
+        gender :"Male",
+        marks: "80"
+       },
+      ,
+      {
+        id: "3",
+        name: "Peter",
+        class: "12th",
+        gender :"Male",
+        marks: "70"
+       },
+       {
+        id: "4",
+        name: "Kajal",
+        class: "12th",
+        gender :"Female",
+        marks: "70"
+       },
+       {
+        id: "5",
+        name: "John",
+        class: "9th",
+        gender :"Male",
+        marks: "40"
+       },
+       {
+       id: "6",
+       name: "Roma Roy",
+       class: "10th",
+       gender :"Female",
+       marks: "90"
+      },
+      {
+      id: "7",
+      name: "Raj Gupta",
+      class: "12th",
+      gender :"Male",
+      marks: "49"
+     },
+     {
+     id: "8",
+     name: "Alex",
+     class: "8th",
+     gender :"Male",
+     marks: "67"
+    },
+    {
+        id: "9",
+        name: "Sophia",
+        class: "10th",
+        gender :"Female",
+        marks: "80"
+       },
+       {
+        id: "10",
+        name: "Vikash Ray",
+        class: "8th",
+        gender :"Male",
+        marks: "71"
+       }, 
+       {
+        id: "11",
+        name: "Noor Khan",
+        class: "12th",
+        gender :"Male",
+        marks: "85"
+       }, 
+       {
+        id: "12",
+        name: "Aviraj Singh",
+        class: "10th",
+        gender :"Male",
+        marks: "60"
+       },
+       {
+        id: "13",
+        name: "Emma",
+        class: "12th",
+        gender :"Female",
+        marks: "78"
+       },
+       {
+        id: "14",
+        name: "Rohan Tyagi",
+        class: "7th",
+        gender :"Male",
+        marks: "55"
+       },
+       {
+        id: "15",
+        name: "Priya Kumari",
+        class: "12th",
+        gender :"Female",
+        marks: "95"
+       }
+]
+export default StudentsData
---
+++ src/components/Tableinfo.js
@@ -0,0 +1,79 @@
+import React,{useState} from 'react'
+import StudentsData from "./StudentsApi";
+import "./style.css";
+const Tableinfo=() =>{
+    const [stuData,setStuData]= useState(StudentsData);
+    const [filterData,setFilterData]=useState("");
+    return (
+        <>
+        <table className="table table-striped mt-5 container">
+  <thead>
+    <tr className="bg-dark text-white">
+      <th scope="col">ID <br />
+      <input type="text" onChange={(e)=>setFilterData(e.target.value)} /></th>
+      <th scope="col">Name<br />
+      <input type="text" onChange={(e)=>setFilterData(e.target.value)} /> </th>
+      <th scope="col">Class<br />
+      <input type="text" onChange={(e)=>setFilterData(e.target.value)} /></th>
+      <th scope="col">Gender<br />
+      <input type="text" onChange={(e)=>setFilterData(e.target.value)} /></th>
+      <th scope="col">Total Marks(%)<br />
+      <input type="text" onChange={(e)=>setFilterData(e.target.value)} /></th>
+    </tr>
+  </thead>
+  <tbody>
+      {
+
+      stuData.filter((currVal)=>{
+        if(filterData===""){
+          return currVal;
+        } else if(
+          currVal.id.toLowerCase().includes(filterData)
+        ) 
+        {
+          return currVal;
+        }
+        else if(
+          currVal.name.toLowerCase().includes(filterData)
+        ) 
+        {
+          return currVal;
+        }
+        else if(
+          currVal.class.toLowerCase().includes(filterData)
+        ) 
+        {
+          return currVal;
+        }
+        else if(
+          currVal.gender.toLowerCase().includes(filterData)
+        ) 
+        {
+          return currVal;
+        }
+        else if(
+          currVal.mareks.toLowerCase().includes(filterData)
+        ) 
+        {
+          return currVal;
+        }
+      })
+      .map((currEle)=>{
+       return <tr key={currEle.id}>
+       <th scope="row">{currEle.id}</th>
+       <td>{currEle.name}</td>
+       <td>{currEle.class}</td>
+       <td>{currEle.gender}</td>
+       <td>{currEle.mareks}</td>
+     </tr>
+      })
+      }
+   
+  </tbody>
+</table>
+   
+        </>
+    )
+}
+
+export default Tableinfo;
